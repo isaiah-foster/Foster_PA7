cf58defa89f80f16bc9a008172aea67e111eb863
Isaiah Foster <isaiahfoster@gmail.com>
2025-03-24 13:52:29 -0700

    beginner files

---
+++ Foster_PA7/main.cpp
@@ -0,0 +1,15 @@
+/*File: main.cpp
+* Author: Isaiah Foster
+* Professor: Andy O'fallon
+* Class: Cpts_122
+* Assignment: PA6
+* Created: 03/24/2025
+* Last updated: 03/24/2025
+* Description: 
+*/
+
+int main(void)
+{
+
+	return 0;
+}
